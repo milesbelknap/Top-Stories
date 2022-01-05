546e098f21280b2969ad9efb125fbb2f1965e46f
Miles Belknap <milesbelknap@hotmail.com>
2022-01-05 13:48:55 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Miles Belknap on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
